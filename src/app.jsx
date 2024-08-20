bbda68168c483730141aa708031084259eb1eeab
Arthur Jacques <arthurjacquesdecastro@gmail.com>
2024-08-20 09:09:44 -0300

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import Paragrafo from "./components/paragrafo/paragrafo"
+import React from "react";
+function App() {
+
+  return (
+    <>
+      <Paragrafo />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/paragrafo.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Paragrafo extends React.Component {
+    render(){
+        return(
+            <div className='paragrafo'>
+               <h3>luihvljkn</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis architecto dicta ut explicabo hic beatae tempore dolores! 
+                   Totam, accusantium cumque, quisquam sed, rerum optio rem odio nemo sequi quo libero.
+                </p>
+            </div>
+        )
+    }
+}
+export default Paragrafo
